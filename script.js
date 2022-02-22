9b4eaa3243fe9f82de2bcdc539d07e8337604ac9
Thomas Pendleton <thomas6576@yahoo.com>
2022-02-21 16:44:00 -0800

    completed drawing app

---
+++ script.js
@@ -0,0 +1,87 @@
+const increaseBtn = document.getElementById('increase')
+const decreaseBtn = document.getElementById('decrease')
+const sizeEl = document.getElementById('size')
+const colorBtn = document.getElementById('color')
+const clearBtn = document.getElementById('clear')
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
+let size = 10
+let color = 'black'
+let x
+let y
+let isPressed = false
+
+canvas.addEventListener('mousedown', (e) => {
+  isPressed = true
+  x = e.offsetX
+  y = e.offsetY
+  //   console.log(isPressed, x, y)
+})
+
+canvas.addEventListener('mouseup', (e) => {
+  isPressed = false
+  x = undefined
+  y = undefined
+  //   console.log(isPressed, x, y)
+})
+
+canvas.addEventListener('mousemove', (e) => {
+  if (isPressed) {
+    const x2 = e.offsetX
+    const y2 = e.offsetY
+    // console.log(x2, y2)
+    drawCircle(x2, y2)
+    drawLine(x, y, x2, y2)
+    x = x2
+    y = y2
+  }
+})
+
+function drawCircle(x, y) {
+  ctx.beginPath()
+  ctx.arc(x, y, size, 0, Math.PI * 2, true)
+  ctx.fillStyle = color
+  ctx.fill()
+}
+
+function drawLine(x1, y1, x2, y2) {
+  ctx.beginPath()
+  ctx.moveTo(x1, y1)
+  ctx.lineTo(x2, y2)
+  ctx.strokeStyle = color
+  ctx.lineWidth = size * 2
+  ctx.stroke()
+}
+
+function updateSizeOnScreen() {
+  sizeEl.innerText = size
+}
+
+colorBtn.addEventListener('change', (e) => (color = e.target.value))
+
+increaseBtn.addEventListener('click', () => {
+  if (size < 50) {
+    size += 5
+    updateSizeOnScreen()
+    decreaseBtn.style.backgroundColor = 'white'
+  }
+  if (size === 50) {
+    increaseBtn.style.backgroundColor = 'grey'
+  }
+})
+
+decreaseBtn.addEventListener('click', () => {
+  if (size > 5) {
+    size -= 5
+    updateSizeOnScreen()
+    increaseBtn.style.backgroundColor = 'white'
+  }
+  if (size === 5) {
+    decreaseBtn.style.backgroundColor = 'gray'
+  }
+})
+
+clearBtn.addEventListener('click', () => {
+  ctx.clearRect(0, 0, canvas.width , canvas.height)
+})
